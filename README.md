291ee2a5cb65f844776c72bc5cb4cd3c0f969c59
Muhammad Kamran <smkamranqadri@yahoo.com>
2017-08-25 15:45:33 +0500

    CRUD added for Todo.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# sls-offline-with-dynamodb-hello-world
-Serverless hello world project for testing offline support with dynamodb
+## sls-offline-with-dynamodb-todo
+Serverless todo project for testing offline support with dynamodb
+
+## Steps to start project
+ 
+**Prerequisite : Java Runtime Engine (JRE) version 6.x or newer**
+
+1. npm install -g serverless
+2. npm install -S serverless-offline
+3. npm install -S serverless-dynamodb-local
+4. serverless dynamodb install
+5. serverless offline start
+
+> Now server started on localhost:3000
--- src/handler.ts
+++ src/handler.ts
@@ -3,11 +3,12 @@ import dynamodb from './dynamodb';
 
 declare var process;
 
-export function hello(event, context, callback) {
+export function addTodo(event, context, callback) {
 
     const timestamp = new Date().getTime();
+    const { text } = JSON.parse(event.body);
     const data = {
-        text: 'hello world'
+        text
     };
 
     if (!data.text) {
@@ -22,7 +23,7 @@ export function hello(event, context, callback) {
     }
 
     const params = {
-        TableName: process.env.DYNAMODB_TABLE,
+        TableName: process.env.TODO_DYNAMODB_TABLE,
         Item: {
             id: uuid.v1(),
             text: data.text,
@@ -41,6 +42,148 @@ export function hello(event, context, callback) {
         const response = {
             statusCode: 200,
             body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+    });
+
+}
+
+export function updateTodo(event, context, callback) {
+
+    const timestamp = new Date().getTime();
+    const { text, checked } = JSON.parse(event.body);
+    const data = {
+        id: event.pathParameters.id,
+        text,
+        checked
+    };
+
+    if (!data.text) {
+        console.error('Missing Data');
+        callback(new Error('Couldn\'t process the data is missing.'));
+        return;
+    }
+    if (typeof data.text !== 'string' || typeof data.checked !== 'boolean') {
+        console.error('Validation Failed');
+        callback(new Error('Couldn\'t process the data is invalid.'));
+        return;
+    }
+
+    const params = {
+        TableName: process.env.TODO_DYNAMODB_TABLE,
+        Key: {
+            id: data.id,
+        },
+        ExpressionAttributeNames: {
+            '#todo_text': 'text',
+        },
+        ExpressionAttributeValues: {
+            ':text': data.text,
+            ':checked': data.checked,
+            ':updatedAt': timestamp,
+        },
+        UpdateExpression: 'SET #todo_text = :text, checked = :checked, updatedAt = :updatedAt',
+        ReturnValues: 'ALL_NEW',
+    };
+
+    dynamodb.update(params, (error, result) => {
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t put in database.'));
+            return;
+        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Attributes),
+        };
+        callback(null, response);
+    });
+
+}
+
+export function deleteTodo(event, context, callback) {
+
+    const data = {
+        id: event.pathParameters.id
+    };
+
+    const params = {
+        TableName: process.env.TODO_DYNAMODB_TABLE,
+        Key: {
+            id: data.id,
+        },
+    };
+
+    // delete the todo from the database
+    dynamodb.delete(params, (error) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t remove the todo item.'));
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify({}),
+        };
+        callback(null, response);
+    });
+
+}
+
+export function getTodo(event, context, callback) {
+
+    const data = {
+        id: event.pathParameters.id
+    };
+
+    const params = {
+        TableName: process.env.TODO_DYNAMODB_TABLE,
+        Key: {
+            id: data.id,
+        },
+    };
+
+    // fetch todo from the database
+    dynamodb.get(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t fetch the todo item.'));
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item),
+        };
+        callback(null, response);
+    });
+
+}
+
+export function getTodos(event, context, callback) {
+
+    const params = {
+        TableName: process.env.TODO_DYNAMODB_TABLE,
+    };
+
+    // fetch all todos from the database
+    dynamodb.scan(params, (error, result) => {
+        // handle potential errors
+        if (error) {
+            console.error(error);
+            callback(new Error('Couldn\'t fetch the todos.'));
+            return;
+        }
+
+        // create a response
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Items),
         };
         callback(null, response);
     });
